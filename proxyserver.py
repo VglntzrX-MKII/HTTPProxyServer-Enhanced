f3b5aeec1ac885a5bcde4d76248081c34a88eaec
Navindren Letchumanan <136433170+VglntzrX-MKII@users.noreply.github.com>
2023-06-26 14:58:11 +0800

    Ingest from master branch (Python2) and initial logic conversion to Python 3.

--- proxyserver.py
+++ proxyserver.py
@@ -1,9 +1,9 @@
-# Simple proxy server for Class assignment
+# Simple proxy server HTTP
 from socket import *
 import sys
 
 if len(sys.argv) <= 1:
-    print 'Usage: "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address of the Proxy Server'
+    print('Usage: "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address of the Proxy Server')
     sys.exit(2)
 
 # Create a server socket, bind it to a port and start listening
@@ -14,24 +14,24 @@ tcpSerSock = socket(AF_INET, SOCK_STREAM)
 #fill start
 # Prepare a server socket
 serverip = sys.argv[1] #from argument
-print serverip
+print(serverip)
 tcpSerSock.bind((serverip, tcpSerPort))
 tcpSerSock.listen(5)
 #fill end
 
 while True:
     # Start receiving data from the client
-    print 'Ready to serve...'
+    print('Ready to serve...')
     tcpCliSock, addr = tcpSerSock.accept()
-    print 'Received a connection from: ', addr
+    print('Received a connection from: ', addr)
     
     #fillstart
     message = tcpCliSock.recv(1024)
     #fillend
-    print message
+    print(message)
     
     # Extract the filename from the given message
-    print message.split()[1]
+    print(message.split()[1])
     filename = message.split()[1].partition("/")[2]
     fileExist = "false"
     filetouse = "/" + filename
@@ -40,7 +40,7 @@ while True:
         f = open(filetouse[1:], "r")
         outputdata = f.readlines()
         fileExist = "true"
-        print 'File Exists!'
+        print('File Exists!')
 
         # ProxyServer finds a cache hit and generates a response message
         tcpCliSock.send("HTTP/1.0 200 OK\r\n")
@@ -49,26 +49,26 @@ while True:
         # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             tcpCliSock.send(outputdata[i])
-        print 'Read from cache'
+        print('Read from cache')
 
         # Error handling for file not found in cache
     except IOError:
-        print 'File Exist: ', fileExist
+        print('File Exist: ', fileExist)
         if fileExist == "false":
             # Create a socket on the proxyserver
             #fill start
-            print 'Creating socket on proxyserver'
+            print('Creating socket on proxyserver')
             c = socket(AF_INET, SOCK_STREAM)
             #fill end
             
             hostn = filename.replace("www.", "", 1)
-            print 'Host Name: ', hostn
+            print('Host Name: ', hostn)
             try:
                 # Connect to the socket to port 80
                 #fill start 
                 c.connect((hostn, 80))
                 #fill end
-                print 'Socket connected to port 80 of the host'
+                print('Socket connected to port 80 of the host')
 
                 # Create a temporary file on this socket and ask port 80
                 # for the file requested by the client 
@@ -94,8 +94,8 @@ while True:
                     tcpCliSock.send(i)
 
             except Exception as inst:
-                print 'Illegal request'
-                print inst
+                print('Illegal request')
+                print(inst)
 
         else:
             # HTTP response message for file not found
