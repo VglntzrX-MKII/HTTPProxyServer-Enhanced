25edc4c6b1161d517c8f92346aa38c2281e9f01c
Navindren Letchumanan <136433170+VglntzrX-MKII@users.noreply.github.com>
2023-06-27 22:08:51 +0800

    A Python 2 --> 3 Functional Migration Update
    - Everything seems to be working properly so far. (fingers crossed)
    - Cache files created actually have valid data in them now. (wow!)
    - Tested by using HTTP://neverssl.com on Pale Moon w/ HTTP version forced to HTTP 1.0.

--- HTTPProxyServer-Enhanced.py
+++ HTTPProxyServer-Enhanced.py
@@ -1,6 +1,7 @@
 # Simple HTTP Proxy Server - Enhanced
-from socket import *
+# https://github.com/VglntzrX-MKII/HTTPProxyServer-Enhanced
 
+from socket import *
 import sys
 
 #Initialisation Parameters
@@ -23,7 +24,7 @@ tcpSerSock = socket(AF_INET, SOCK_STREAM)
 
 
 # Prepare a server socket
-
+#serverip = sys.argv[1] #from argument
 print(serverip +":"+ str(tcpSerPort))
 tcpSerSock.bind((serverip, tcpSerPort))
 tcpSerSock.listen(5)
@@ -44,21 +45,23 @@ while True:
     fileExist = "false"
     try:
         # Check whether the file exists in the cache
-        f = open(filename, "r")
-        outputdata = f.readlines()
+        with open(filename, "rb") as f:   #must be rb, else reading cache will cause a crash
+                outputdata = f.readlines()
+
         fileExist = "true"
         print('File Exists!')
 
         # ProxyServer finds a cache hit and generates a response message
-        ok_1 = "HTTP/1.0 200 OK\r\n"
+        ok_1 = "HTTP/1.1 200 OK\r\n"
         ok_2 = "Content-Type:text/html\r\n"
         tcpCliSock.send(ok_1.encode())
         tcpCliSock.send(ok_2.encode())
 
         # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            tcpCliSock.send(outputdata[i].encode())
-        print('Read from cache')
+        for line in outputdata:
+                tcpCliSock.send(line)
+                print('Reading Data from Cache...')
+
 
         # Error handling for file not found in cache
     except IOError:
@@ -74,9 +77,7 @@ while True:
             print('Host Name: ', hostn)
             try:
                 # Connect to the socket to port 80
-
                 c.connect((hostn, 80))
-
                 print('Socket Has Connected to the Host at Port 80')
 
                 c.sendall(message.encode())
@@ -87,8 +88,8 @@ while True:
                 # Create a new file in the cache for the requested file.
                 # Also send the response in the buffer to client socket
                 # and the corresponding file in the cache
-                tmpFile = open("./" + filename, "w")
-                tmpFile.writelines(buff.decode().replace('\r\n', '\n'))
+                tmpFile = open("./" + filename, "wb") #can only be wb, if only use w, will cause empty files to be written
+                tmpFile.write(buff)
                 tmpFile.close()
 
 
@@ -99,10 +100,11 @@ while True:
         else:
             # HTTP response message for file not found
 
-            tcpCliSock.send("HTTP/1.0 404 Not Found\r\n")
+            tcpCliSock.send("HTTP/1.1 404 Not Found\r\n")
             tcpCliSock.send("Content-Type:text/html\r\n")
             
             
     # Close the socket and the server sockets
     tcpCliSock.close()
 tcpSerSock.close()
+
